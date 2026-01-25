eec7bd7e14a4f352af8b6f41ea6824b4a95a6544
Leah <leahas13@gmail.com>
2026-01-25 13:41:14 -0500

    removed figure tags, set up cloudinary src urls, some image styling

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
 let currentPageMain = document.querySelector("main");
-let galleryFigures = document.querySelectorAll(".gallery figure");
+let galleryImages = document.querySelectorAll(".gallery img");
 
 // for each image in a gallery, add an event listener that displays
 // a larger image and details
-galleryFigures.forEach((figure) => {
+galleryImages.forEach((image) => {
     // get current image src
-    let currentImgSrc = figure.children[0].src;
-    let currentImgCaption = figure.children[1].innerText;
+    let currentImgSrc = image.src;
 
-    // create modal on figure click
-    figure.addEventListener("click", () => {
+    // create modal on image click
+    image.addEventListener("click", () => {
         // check if there is currently a modal open and close it if so
         let checkForOpenModal = document.querySelector(".popup");
         if (checkForOpenModal) {
@@ -23,33 +22,61 @@ galleryFigures.forEach((figure) => {
         // add image to modal
         let image = document.createElement("img");
         image.src = currentImgSrc;
+
+        // if landscape orientation, set width
+        if (image.height < image.width) {
+            image.width = "100%";
+            image.style.maxWidth = "900px";
+            image.height = "auto";
+        } 
+        // if portrait orientation, set height
+        else {
+            image.width = "auto";
+            image.height = "100%";
+            image.style.maxHeight = "600px";
+        }
+
         modal.append(image);
 
-        // add description to modal
-        let description = document.createElement("p");
-        description.innerText = currentImgCaption;
-        modal.append(description);
-
-        // add button to modal
+        // add close button to modal
         let closeButton = document.createElement("button");
         closeButton.innerHTML = "X";
+        closeButton.id = "close-popup-button";
+        modal.append(closeButton);
+
+        // flexbox div for forward and backward buttons
+        let carouselNav = document.createElement("div");
+        carouselNav.id = "carousel-nav-container";
+
+        // add backward and forward carousel buttons to div
+        let backwardButton = document.createElement("button");
+        backwardButton.innerHTML = "<";
+
+        let forwardButton = document.createElement("button");
+        forwardButton.innerHTML = ">";
+
+        carouselNav.append(backwardButton, forwardButton);
+        modal.append(carouselNav);
+
         closeButton.addEventListener("click", () => {
             modal.remove();
         });
-        modal.append(closeButton);
+
+        // click anywhere to close modal
+        modal.addEventListener("click", () => {
+            modal.remove();
+        });
 
         // add modal to main
         currentPageMain.append(modal);
     })
 });
 
-galleryFigures.forEach((figure) => {
-    let figImg = figure.children[0];
-
-    // if image is landscape, have the figure 
-    // span the full width of the grid
-    if (figImg.height < figImg.width) {
-        figure.style.gridColumn = "span 2";
+galleryImages.forEach((image) => {
+    // if image is landscape, have the image
+    // span 2 columns in the gallery grid
+    if (image.height < image.width) {
+        image.style.gridColumn = "span 2";
     }
 
 })
